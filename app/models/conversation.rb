35acfb17c909db924b49a87bf71cc272fa241d18
Madhav Paudel <poudelmadhav143@gmail.com>
2019-07-29 16:16:41 +0545

    conversation model actions added

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,4 +4,10 @@ class Conversation < ApplicationRecord
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
 
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+
+  validates :sender_id, uniqueness: { scope: :recipient_id }
+
+  def opposed_user(user)
+    user == recipient ? sender : recipient
+  end
 end
